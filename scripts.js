462bc7a02f11620567af9e31033613cfaba7b2a0
Maciej Okrajek <maciej.okrajek@shoper.pl>
2022-10-18 21:21:42 +0200

    testowy commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function zdejmowanieKlasy() {
+    let usuwanie = document.getElementById("hide");
+    let dodawanie = document.getElementById("remove_class_box");
+    usuwanie.classList.remove("hidden");
+    dodawanie.classList.add("hidden");
+}
